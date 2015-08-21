645cb21939a52a9f03fa2a61e3acbee3a70a4a23
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2015-08-21 19:36:42 -0400

    update test scripts
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- loops/loop-session.sh
+++ loops/loop-session.sh
@@ -1,15 +1,17 @@
-#!/bin/sh
+#!/bin/bash
+
+NUM=$1
 
 for a in $(seq 1 1000000); do
-	lttng create testrelay01 -U net://localhost --live
+	lttng create testrelay${NUM} -U net://localhost --live
 	# TODO: test with no rotation.
-	#lttng enable-channel -u -s testrelay01 chan1
+	#lttng enable-channel -u -s testrelay${NUM} chan1
 	# TODO: test with more files in rotation.
-	#lttng enable-channel -u -s testrelay01 --subbuf-size 4096 -C 4096 -W 32 chan1
-	lttng enable-channel -u -s testrelay01 --subbuf-size 4096 -C 4096 -W 2 chan1
-	lttng enable-event -u -a -c chan1 -s testrelay01
-	lttng start testrelay01
+	#lttng enable-channel -u -s testrelay${NUM} --subbuf-size 4096 -C 4096 -W 32 chan1
+	lttng enable-channel -u -s testrelay${NUM} --subbuf-size 4096 -C 4096 -W 2 chan1
+	lttng enable-event -u -a -c chan1 -s testrelay${NUM}
+	lttng start testrelay${NUM}
 	# TODO: also test without sleep
 	sleep 1
-	lttng destroy testrelay01
+	lttng destroy testrelay${NUM}
 done
--- loops/loop-view.sh
+++ loops/loop-view.sh
@@ -1,10 +1,13 @@
-#!/bin/sh
+#!/bin/bash
+
+NUM=$1
 
 for a in $(seq 1 1000000); do
 	echo "Viewer loop begin"
-	babeltrace -v -i lttng-live net://localhost/host/thinkos/testrelay01 > /dev/null
+	#babeltrace -v -i lttng-live net://localhost/host/${HOSTNAME}/testrelay${NUM} > /dev/null
+	babeltrace -v -i lttng-live net://localhost/host/${HOSTNAME}/testrelay${NUM} > /dev/null
 	# TODO: also test with sleep
 	#sleep 1
 	# TODO: also test without -v (for speed)
-	#babeltrace -i lttng-live net://localhost/host/thinkos/testrelay01
+	#babeltrace -i lttng-live net://localhost/host/${HOSTNAME}/testrelay${NUM}
 done
