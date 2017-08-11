eafc587591d1c45f48313314cbf5ccc26f1af773
Jonathan Rajotte <jonathan.rajotte-julien@efficios.com>
2017-08-11 13:51:59 -0400

    Use -n to prevent sessiond spawning
    Signed-off-by: Jonathan Rajotte <jonathan.rajotte-julien@efficios.com>
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- loops/loop-session.sh
+++ loops/loop-session.sh
@@ -3,15 +3,15 @@
 NUM=$1
 
 for a in $(seq 1 1000000); do
-	lttng create testrelay${NUM} -U net://localhost --live
+	lttng -n create testrelay${NUM} -U net://localhost --live
 	# TODO: test with no rotation.
 	#lttng enable-channel -u -s testrelay${NUM} chan1
 	# TODO: test with more files in rotation.
 	#lttng enable-channel -u -s testrelay${NUM} --subbuf-size 4096 -C 4096 -W 32 chan1
-	lttng enable-channel -u -s testrelay${NUM} --subbuf-size 4096 -C 4096 -W 2 chan1
-	lttng enable-event -u -a -c chan1 -s testrelay${NUM}
-	lttng start testrelay${NUM}
+	lttng -n enable-channel -u -s testrelay${NUM} --subbuf-size 4096 -C 4096 -W 2 chan1
+	lttng -n enable-event -u -a -c chan1 -s testrelay${NUM}
+	lttng -n start testrelay${NUM}
 	# TODO: also test without sleep
 	sleep 1
-	lttng destroy testrelay${NUM}
+	lttng -n destroy testrelay${NUM}
 done
